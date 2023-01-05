2b8d0fc7ffe0e96da99170aeb1953a48bff15435
ErikAndresFelea <erikandresfelea@gmail.com>
2023-01-05 11:55:03 +0100

    Database connection

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 // We get Node modules we need and save it on a variable
-// Create a express app 
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+// Database URL (provided by MongoDB webpage)
+const uri = 'mongodb+srv://Argnos:Password2@cluster0.czjf5bw.mongodb.net/?retryWrites=true&w=majority';
+
+// Create a express app and conects to database
 const app = express();
+mongoose.connect(uri)
+    .then( db => console.log('Connected to MongoDB'))
+    .catch(err => console.error(err));
 
 // Settings
 app.set('port', process.env.PORT || 8080);
@@ -12,7 +20,8 @@ app.use(morgan('dev'));     // Get info from browser request
 app.use(express.json());    // Allows server to understand json data
 
 // Routes
-app.use('/tasks', require('.\\routes\\tasks'))
+app.use('/tasks', require('.\\routes\\tasks'));
+
 
 // Static files (html, css, images, ...)
 app.use(express.static(__dirname + '\\public'));
