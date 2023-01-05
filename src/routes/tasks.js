ed3caef77d6a91b3c08439d7303ddd94cf8a2b19
ErikAndresFelea <erikandresfelea@gmail.com>
2023-01-05 11:54:33 +0100

    -

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -4,7 +4,7 @@ const router = express.Router();
 // Set a response for a specific route
 router.get('/', (req, res) => {
     res.send('API Tasks comes here')
-})
+});
 
 // Export the routes
 module.exports = router;
