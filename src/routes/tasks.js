99599f2f15155f38a2310702b92f659519ea4db0
ErikAndresFelea <erikandresfelea@gmail.com>
2023-01-09 09:38:26 +0100

    CRUD operations

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -1,9 +1,32 @@
 const express = require('express');
+const { findByIdAndUpdate } = require('..\\models\\task');
 const router = express.Router();
 
-// Set a response for a specific route
-router.get('/', (req, res) => {
-    res.send('API Tasks comes here')
+const Task = require('..\\models\\task');
+
+// Get all taks from db
+router.get('/', async (req, res) => {
+    const tasks = await Task.find();
+    res.json(tasks);
+});
+
+// Post a task
+router.post('/', async (req, res) => {
+    const task = new Task(req.body);
+    await task.save();
+    res.json({ status: 'Task saved' });
+});
+
+// Update a task by id
+router.put('/:id', async (req, res) => {
+    await Task.findByIdAndUpdate(req.params.id, req.body);
+    res.json({ status: 'Task updated' });
+});
+
+// Delete a task by id
+router.delete('/:id', async (req, res) => {
+    await Task.findByIdAndRemove(req.params.id);
+    res.json({ status: 'Task deleted' });
 });
 
 // Export the routes
